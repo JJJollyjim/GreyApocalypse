784e736a1d770c629b79b480cfd16cf393e2b111
Tobias Heitland <tobias.heitland@me.com>
2013-09-15 15:56:29 +1200

    Implemented loading programs from local storage

--- GreyApocalypse/scripts/initialisation.js
+++ GreyApocalypse/scripts/initialisation.js
@@ -1,11 +1,16 @@
 // This module creates and returns a function that initialises the game
 
-define(['evaluator/initialiseEnvironment', 'ui/buttonHandlers/initButtonHandlers', 'ui/drawUI'], function (initEnv, initButtonHandlers, drawUI) {
+define(['evaluator/initialiseEnvironment', 'ui/buttonHandlers/initButtonHandlers', 'ui/drawUI', 'jquery'], function (initEnv, initButtonHandlers, drawUI, $) {
 	return function () {
-		//initEnv('infinite { +> }', 'infinite { repeat (10) { +> } v }')
 		initEnv('.', '.');
 
 		initButtonHandlers();
 		drawUI();
+
+		if (localStorage.program1)
+			$('#p1textarea').val(localStorage.program1);
+		if (localStorage.program2)
+			$('#p2textarea').val(localStorage.program2);
+		};
 	}
 })
