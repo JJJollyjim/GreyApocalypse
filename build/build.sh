8d3bbb5130d021b184a182d4c168798c53c385ee
Tobias Heitland <tobias.heitland@me.com>
2013-09-24 20:03:20 +1200

    Fixed the syntax for including require.js in the uglified file

--- build/build.sh
+++ build/build.sh
@@ -1,3 +1,3 @@
 node r.js -o build.js
-sed 's/<script data-main="scripts\/main" src="scripts\/lib\/require.js"><\/script>/<script type="test\/javascript" src="main-built.js"><\/script>/g' ../GreyApocalypse/index.html > index.html
+sed 's/<script data-main="scripts\/main" src="scripts\/lib\/require.js"><\/script>/<script data-main="main-built.js" src="..\/GreyApocalypse\/scripts\/lib\/require.js"><\/script>/g' ../GreyApocalypse/index.html > index.html
 cp ../GreyApocalypse/css/style.css ./css/style.css
