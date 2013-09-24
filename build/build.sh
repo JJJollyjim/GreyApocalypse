745028d66d723ffb99a825dd91a5667a3a874e2b
Tobias Heitland <tobias.heitland@me.com>
2013-09-23 21:15:51 -0400

    Updated build script to copy css file

--- build/build.sh
+++ build/build.sh
@@ -1,2 +1,3 @@
 node r.js -o build.js
 sed 's/<script data-main="scripts\/main" src="scripts\/lib\/require.js"><\/script>/<script type="test\/javascript" src="main-built.js"><\/script>/g' ../GreyApocalypse/index.html > index.html
+cp ../GreyApocalypse/css/style.css ./css/style.css
