df23a984890826cea5052dfb3bf5a581b656be4c
Tobias Heitland <tobias.heitland@me.com>
2013-09-23 21:11:10 -0400

    Fixed build script

--- build/build.sh
+++ build/build.sh
@@ -1,1 +1,2 @@
 node r.js -o build.js
+sed 's/<script data-main="scripts\/main" src="scripts\/lib\/require.js"><\/script>/<script type="test\/javascript" src="main-built.js"><\/script>/g' ../GreyApocalypse/index.html > index.html
